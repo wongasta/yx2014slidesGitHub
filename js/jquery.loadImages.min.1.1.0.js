64106c0f2c1dcc871f50fc07a547d3dd3084c3fe
loldongs <yxia@utexas.edu>
2014-05-11 20:15:38 -0500

    Upload all files
    Upload all completed files

---
+++ js/jquery.loadImages.min.1.1.0.js
@@ -0,0 +1,1 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(jQuery);
